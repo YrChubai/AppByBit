562b964c06f753a229955ddcbcf69e1589c60e96
YrChubai <razawchs@gmail.com>
2024-02-13 22:05:28 +0300

    Edit Announcements model

--- app/src/main/java/com/example/appbybit/data/models/Announcements.kt
+++ app/src/main/java/com/example/appbybit/data/models/Announcements.kt
@@ -1,16 +1,13 @@
 package com.example.appbybit.data.models
 
-import java.sql.Timestamp
-
 data class Announcements(
     val retCode: Int,
     val retMsg: String,
     val result: ResultItem,
     val retExtInfo: ExtInfo,
-    val time: Timestamp
+    val time: Long
 )
 
 class ExtInfo(
 
 )
-
