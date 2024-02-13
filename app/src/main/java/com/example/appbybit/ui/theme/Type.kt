8ea1f93ac85f3b7fb4ca91530256627623914b76
YrChubai <razawchs@gmail.com>
2024-02-13 21:56:32 +0300

    Add ImbplexSans Font

--- app/src/main/java/com/example/appbybit/ui/theme/Type.kt
+++ app/src/main/java/com/example/appbybit/ui/theme/Type.kt
@@ -2,14 +2,23 @@ package com.example.appbybit.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.appbybit.R
+
+val IBMPlexSans = FontFamily(
+    Font(R.font.imbplexsans_medium, FontWeight.Medium),
+    Font(R.font.imbplexsans_regular, FontWeight.Normal),
+    Font(R.font.imbplexsans_semibold, FontWeight.SemiBold),
+)
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = IBMPlexSans,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
