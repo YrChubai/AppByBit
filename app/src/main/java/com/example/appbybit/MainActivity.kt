e5d16aaa04786f8690b3d6dd09305d0b7d2292ee
YrChubai <razawchs@gmail.com>
2024-02-13 22:06:47 +0300

    Add navController and Start Screen

--- app/src/main/java/com/example/appbybit/MainActivity.kt
+++ app/src/main/java/com/example/appbybit/MainActivity.kt
@@ -3,23 +3,25 @@ package com.example.appbybit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.appbybit.ui.theme.AppByBitTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.appbybit.screen.MainScreen
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LazyColumn{
-            }
+            val navController = rememberNavController()
+            NavHost(
+                navController = navController,
+                startDestination = "route_screen", builder ={
+                    composable("route_screen") {
+                        MainScreen(navController)
+                    }
+                }
+            )
         }
     }
 }
-
